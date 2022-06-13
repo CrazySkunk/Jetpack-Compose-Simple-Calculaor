77b989456ea07f1d1ece70cadf8465e502c53921
Jamie Codez <cruiseomondi90@gmail.com>
2022-06-13 16:29:49 +0300

    Initial commit

---
+++ app/src/main/java/com/jamie/code/jetpackcomposeapp/CalculatorState.kt
@@ -0,0 +1,7 @@
+package com.jamie.code.jetpackcomposeapp
+
+data class CalculatorState(
+    val number1: String = "",
+    val number2: String = "",
+    val operation: CalculatorOperation? = null
+)
